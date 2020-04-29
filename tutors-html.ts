ad541e56b924e2987ddfb4e278d11b101453e252
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-29 09:33:38 +0100

    version number management

--- tutors-html.ts
+++ tutors-html.ts
@@ -3,8 +3,10 @@ import * as fs from 'fs';
 import { Course } from '@tutors-sdk/tutors-lib/src/models/course';
 import { HtmlEmitter } from './src/controllers/html-emitter';
 import { copyFolder } from '@tutors-sdk/tutors-lib/src/utils/futils';
-const version = require('./package.json').version;
+const tutors = require('./package.json').version;
+const tutors_lib = require('./package.json').dependencies['@tutors-sdk/tutors-lib'];
 
+const version = `tutors-json ${tutors} (tutors-lib: ${tutors_lib})`;
 const nunjucks = require('nunjucks');
 const root = __dirname;
 nunjucks.configure(root + '/src/viewskit', { autoescape: false });
@@ -13,12 +15,12 @@ nunjucks.installJinjaCompat();
 if (fs.existsSync('course.md')) {
   let site = 'public-site';
   const course = new Course();
-  console.log(`tutors-html static course generator ${version}`);
+  console.log(`Static course generator ${version}`);
   course.publish(site);
   const emitter = new HtmlEmitter();
   emitter.generateCourse(site, course);
-  console.log(`tutors-html ${version}`);
+  console.log(`${version}`);
   copyFolder(`${root}/src/assets`, site);
 } else {
-  console.log('Cannot locate course.md or portfolio.yaml. Change to course folder and try again. ');
+  console.log('Cannot locate course.md. Change to course folder and try again. ');
 }
